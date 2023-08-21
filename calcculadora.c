d17b7be4fe3595cf5e502e0cfe02e355c5b292e2
mariana2603 <marissp_@hotmail.com>
2023-08-21 01:30:07 -0300

    Update calcculadora.c
    divisao

--- calcculadora.c
+++ calcculadora.c
@@ -1,3 +1,16 @@
+#include <stdio.h>
+
+float CalcDivisao(float n1, float n2){
+    printf("\nNumero 1:");
+    scanf("%f", &n1);
+       
+    printf("\nNumero 2:");
+    do {
+        scanf("%f", &n2); 
+    } while(n2 == 0);
+       
+    return n1 / n2;
+}
 int main(){
     float n1, n2;
     float resultado; 
